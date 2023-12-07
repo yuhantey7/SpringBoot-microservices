a4790c3dbc4794e49967949a427b21d1402973cf
yuhantey <yuhantey@digitalroute.com>
2023-12-07 19:42:14 +0800

    WIP-1207- Add missed file

--- section2/accounts/src/main/java/com/eazybank/accounts/repository/AccountsRepository.java
+++ section2/accounts/src/main/java/com/eazybank/accounts/repository/AccountsRepository.java
@@ -5,6 +5,10 @@ import com.eazybank.accounts.controller.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AccountsRepository extends JpaRepository<Accounts, Long > {
+
+    Optional<Accounts> findByCustomerId(Long customerId); 
 }
