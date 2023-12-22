59777f55b0392b2a0ee7b3c22a1a68fe7271e93c
yuhantey <yuhantey@digitalroute.com>
2023-12-22 10:52:50 +0800

    WIP-1222 - Work for entity class v2

--- section2/loans/src/main/java/com/eazybank/loans/entity/BaseEntity.java
+++ section2/loans/src/main/java/com/eazybank/loans/entity/BaseEntity.java
@@ -1,2 +1,38 @@
-package com.eazybank.loans.entity;public class BaseEntity {
+package com.eazybank.loans.entity;
+
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+/**
+ * AuditingEntityListener - To use @CreatedBy , @LastModifiedBy , @CreatedDate , @LastModifiedDate
+ */
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter @Setter @ToString
+public class BaseEntity {
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @CreatedBy
+    private String createdBy;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    @LastModifiedBy
+    private String updatedBy;
+
 }
--- section2/loans/src/main/java/com/eazybank/loans/entity/Loans.java
+++ section2/loans/src/main/java/com/eazybank/loans/entity/Loans.java
@@ -1,2 +1,25 @@
-package com.eazybank.loans.entity;public class Loans {
+package com.eazybank.loans.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+
+public class Loans extends BaseEntity{
+
+        @Id
+        @Column(name="loan_id")
+        private int loanId;
+        @Column(name="mobile_number")
+        private String mobileNumber;
+        @Column(name="loan_number")
+        private String loanNumber;
+        @Column(name="loan_type")
+        private String loanType;
+        @Column(name="total_loan")
+        private int totalLoan;
+        @Column(name="amount_paid")
+        private int amountPaid;
+        @Column(name="outstanding_amount")
+        private int outstandingAmount;
+
 }
